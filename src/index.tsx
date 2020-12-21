fd49a58011364395bedefb8332734a81797b234f
William Morris <morrisgwilliam@gmail.com>
2020-12-21 12:35:59 -0800

    :construction: added material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './store/store'
+import store from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <Router>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
     </Provider>
-  </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
